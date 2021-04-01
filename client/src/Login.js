6bcbedb14250a1d50784918a51180ebe1854f226
egor-shell <shell.egor.jesus@gmail.com>
2021-04-01 23:09:02 +0600

    Setting axios

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,21 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Axios from "axios";
 
 function Login() {
-    Axios({
-        method: "GET",
-        url: "http://localhost:3001/",
-        headers: {
-            "Content-Type": "application/json"
-        }
-    }).then(res => {
-        console.log(res.data.message);
-    });
+
+    // const msg = () => {
+    //     Axios({
+    //         method: "GET",
+    //         url: "http://localhost:3001/",
+    //         headers: {
+    //             "Content-Type": "application/json"
+    //         }
+    //     }).then(res => {
+    //         return res.data.message
+    //     });
+    // }
+    const url = "http://localhost:3001/"
+    function msg() {
+        return Axios.get(url).then(res => {
+            return res.data.message
+        })
+    }
 
     return (
         <div className="auth">
-            <input type="text" />
-            <button></button>
+            {/* <input type="text" /> */}
+            <p>Кликни на кнопку</p>
+            <button onClick={() => msg()}>
+                Нажми на меня
+            </button>
         </div>
     );
 }
